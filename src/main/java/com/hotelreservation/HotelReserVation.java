49537b37e00fb9a316ce373803f9c9cd9c8dabf0
1501299026 <sinumsd98@gmail.com>
2021-06-01 20:11:39 -0700

    ADDED: Welcome to Hotel Reservation System

---
+++ src/main/java/com/hotelreservation/HotelReserVation.java
@@ -0,0 +1,8 @@
+package com.hotelreservation;
+
+public class HotelReserVation {
+    public static void main(String[] args) {
+        System.out.println("Welcome to hotel reservation system")
+
+    }
+}
